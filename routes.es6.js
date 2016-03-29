1bc42091dcf32fbf3b5efbfc9ba3dff194448358
Jack Lawson <ajacksified@gmail.com>
2016-03-28 17:35:58 -0700

    Proper id handling

--- routes.es6.js
+++ routes.es6.js
@@ -4,19 +4,27 @@ const { NotImplementedError, ResponseError, DisconnectedError } = errors;
 export default function routes (app) {
   const { router } = app;
 
-  router.all('/:endpoint', async (ctx) => {
-    const method = ctx.req.method.toLowerCase();
+  router.all('/:endpoint/:id(.*)?', async (ctx) => {
+    let method = ctx.req.method.toLowerCase();
+    if (method === 'delete') { method = 'del'; }
+
     const endpoint = ctx.params.endpoint;
+    const id = ctx.params.id;
 
     if (!ctx.api[endpoint]) {
       ctx.status = 404;
       return;
     }
 
+    const data = ['post', 'put', 'patch'].includes(method) ? ctx.request.body : ctx.query;
+
+    if (id) { data.id = id; }
+
     try {
-      ctx.body = await ctx.api[endpoint][method](ctx.body || ctx.query);
+      ctx.body = await ctx.api[endpoint][method](data);
     } catch (e) {
       ctx.status = e.status || 500;
+      ctx.body = 'ERROR';
 
       if (e instanceof NotImplementedError) {
         ctx.status = 405;
@@ -34,7 +42,6 @@ export default function routes (app) {
         return;
       }
 
-      ctx.body = 'ERROR';
       return;
     }
   });
